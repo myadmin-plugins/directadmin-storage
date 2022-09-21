4757562ecfa5f68a6dbbd0d5e2346a50a66d4a19
Ganesh R <kumar.interserver@gmail.com>
2022-09-21 23:31:49 +0530

    Re-activate unsuspend not working - testing/fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -204,7 +204,7 @@ class Plugin
             $apiCmd = '/CMD_API_SELECT_USERS';
             $apiOptions = [
                 'location' => 'CMD_SELECT_USERS',
-                'suspend' => 'Unsuspend',
+                'suspend' => 'unsuspend',
                 'select0' => $serviceClass->getUsername()
             ];
             $sock->query($apiCmd, $apiOptions);
